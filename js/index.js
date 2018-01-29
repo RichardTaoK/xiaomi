03a24ff5911022d6b9ca21e94a5b96cc0850ce06
2579312470@qq.com <2579312470@qq.com>
2018-01-30 00:11:00 +0800

    xiaomi

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 window.onload=function(){
-	// 选项卡
+// 选项卡
 	let list=document.querySelectorAll('.list li');
 	let right =document.querySelectorAll('.xia ul.right');
 	// console.dir(right.length);
@@ -14,19 +14,47 @@ window.onload=function(){
 			right[index].classList.add('active');
 		}
 	})
-	// banner
+
+// 封装轮播
+	// let k=0;
+	// function kk(a,b){
+	// 	if(a instanceof NodeList){
+	// 		k++;
+	// 		if(k>=a.length){k=0;}
+	// 		a.forEach(function(val){
+	// 			val.classList.remove(b);
+	// 		})
+	// 		a[k].classList.add(b);
+	// 	}
+	// 	return function(){kk(a,b);}
+	// }
+	// function dianji(a,b){
+	// 	if(a instanceof NodeList){
+	// 		k--;
+	// 		if(k<0){k=a.length-1;}
+	// 		a.forEach(function(val){
+	// 			val.classList.remove(b);
+	// 		})
+	// 		a[k].classList.add(b);
+	// 	}
+	// }
+//结束
+// banner
 	let n=0;
-	let banner=document.querySelectorAll('.banner_top li');
+	let banner=document.querySelectorAll('.banner_top .list');
 	let banner_t=document.querySelector('.banner_top');
 	let left=document.querySelector('.banner_top .left');
-	let right_1=document.querySelector('.banner_top .right');
+	let right1=document.querySelector('.banner_top .right');
+	let d=document.querySelectorAll('.banner_top .dian .d');
 	function move(){
 		n++;
 		if(n>=banner.length){n=0;}
-		banner.forEach(function(val){
+		banner.forEach(function(val,index){
 			val.classList.remove('move');
+			d[index].classList.remove('active');
 		})
 		banner[n].classList.add('move');
+		d[n].classList.add('active');
 	}
 	let t=setInterval(move,2000);
 	banner_t.onmouseover=function(){
@@ -35,7 +63,7 @@ window.onload=function(){
 	banner_t.onmouseout=function(){
 		t=setInterval(move,2000);
 	}
-	right_1.onclick=function(){
+	right1.onclick=function(){
 		move();
 	}
 	left.onclick=function(){
@@ -46,12 +74,24 @@ window.onload=function(){
 		})
 		banner[n].classList.add('move');
 	}
+	d.forEach(function(value,index){
+		value.onclick=function(){
+			d.forEach(function(a,b){
+				a.classList.remove('active');
+				banner[b].classList.remove('move');
+			})
+			this.classList.add('active');
+			banner[index].classList.add('move');
+			n=index;
+		}
+	})
+	
+	
 //单品banner
 	let box=document.querySelectorAll('.dan_bd .box1');
-	let b=document.querySelector('.dan_bd');
+	let b=document.querySelector('.dan .container');
 	let a=document.querySelector('.more .a');
 	let c=document.querySelector('.more .b');
-
 	let i=0;
 	function dong(){
 		i++;
@@ -80,29 +120,65 @@ window.onload=function(){
 		box[i].classList.add('xian');
 	}
 
+// 图书
+	let box_b=document.querySelectorAll('.box .t1');
+	function fz(box_b){
+		let tushu=box_b.querySelectorAll('.xm ul li');
+		let left_1=box_b.querySelector('.xm ul .left');
+		let right_1=box_b.querySelector('.xm ul .right');
+		let dian=box_b.querySelectorAll('.many .dian .dd');
 
-let tushu=document.querySelectorAll('.xm li');
-// console.log(tushu)
 
-// 封装轮播
-	let k=0;
-	function kk(a,b){
-		if(a instanceof NodeList){
-			k++;
-			// console.log(a.length);
-			if(n>=a.length){k=0;}
-			a.forEach(function(val){
-				val.classList.remove(b);
+		let dn=0;
+		function move2(){
+			dn++;
+		
+
+			if(dn>=tushu.length){dn=0;}
+			tushu.forEach(function(val,index){
+				val.classList.remove('appear');
+				dian[index].classList.remove('dot');
 
 			})
-			a[k].classList.add(b);
-			// console.log(n)
+			tushu[dn].classList.add('appear');
+			dian[dn].classList.add('dot');
 		}
+		let dt=setInterval(move2,2000);
+		banner_t.onmouseover=function(){
+			clearInterval(dt);
+		}
+		banner_t.onmouseout=function(){
+			dt=setInterval(move2,2000);
+		}
+		right_1.onclick=function(){
+			move2();
+		}
+		left_1.onclick=function(){
+			dn--;
+			if(dn<0){dn=tushu.length-1;}
+			tushu.forEach(function(val){
+				val.classList.remove('appear');
+			})
+			tushu[dn].classList.add('appear');
+		}
+		dian.forEach(function(value,index){
+			value.onclick=function(){
+				dian.forEach(function(a,b){
+					a.classList.remove('dot');
+					tushu[b].classList.remove('appear');
+				})
+				this.classList.add('dot');
+				tushu[index].classList.add('appear');
+				dn=index;
+			}
+		})
 	}
+	box_b.forEach(function(val,index){
+		fz(val);
+	})
+	
+	
 
-	// let t=setInterval("move(lis,'active')",2000);
-//结束
-// let tu=setInterval("kk(tushu,'appear')",2000);
 	
 	
 
